992d22356dc5150c0b358b9e975b7fa3fe03caec
Balabol <grustniiii@gmail.com>
2018-12-13 15:41:09 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+var chatRef = new Firebase('https://komvux-firebase11111.firebaseio.com/');
+
+// Create new user
+
+function register() {
+    var username = document.getElementById("registerUsername").value;
+    var password = document.getElementById("registerPassword").value;
+    chatRef.createUser({
+        email    : username,
+        password : password
+    }, function(error, userData) {
+        if (error) {
+            console.log("Error creating user:", error);
+        } else {
+            console.log("Successfully created user account with uid:", userData.uid);
+        }
+    });
+
+}
+
+// Login user
+
+function login() {
+    var username = document.getElementById("loginUsername").value;
+    var password = document.getElementById("loginPassword").value;
+    chatRef.authWithPassword({
+        email    : username,
+        password : password
+    }, function(error, authData) {
+        if (error) {
+            console.log("Login Failed!", error);
+        } else {
+            console.log("Authenticated successfully with payload:", authData);
+        }
+    });
+
+    chatRef.onAuth(function(authData) {
+  // Once authenticated, instantiate Firechat with the user id and user name
+  if (authData) {
+    initChat(authData);
+  }
+});
+
+}
+
+
+function initChat(authData) {
+  var chat = new FirechatUI(chatRef, document.getElementById('firechat-wrapper'));
+  chat.setUser(authData.uid, authData[authData.provider].displayName);
+}
